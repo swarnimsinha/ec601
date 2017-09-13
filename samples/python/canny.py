9fc69bed2d2f66953a1a1bf3304bbc171be7bb35
Swarnim Sinha <swarnims@bu.edu>
2017-09-13 19:54:59 -0400

    Removed matlab plotting and changed them with inbuilt opencv functions

--- samples/python/canny.py
+++ samples/python/canny.py
@@ -1,15 +1,13 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 while True:
 	img = cv2.imread('index.png',0)
 	edges = cv2.Canny(img,100,200)
 
-	plt.subplot(121),plt.imshow(img,cmap = 'gray')
-	plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-	plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-	plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
-	plt.show()
-cv2.destroyAllWindows()
+	cv2.imshow("source", img)
+	k = cv2.waitKey(33)
+	if k == 27:
+		break
+	elif k == 32:
+		cv2.imshow("cannyEdge", edges)
