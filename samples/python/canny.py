8f22d6bb0d61460f379d46d8c5cdd5a5f798514b
Swarnim Sinha <swarnims@bu.edu>
2017-09-17 20:36:25 -0400

    Added a tracker to change the threshold value using nidhi's tracker

--- samples/python/canny.py
+++ samples/python/canny.py
@@ -1,15 +1,33 @@
 import cv2
 #import numpy as np
+canny = 'Canny Image'
+
+# Initialize window to show
+cv2.namedWindow(canny)
+
+# this function is needed for the createTrackbar step downstream
+def nothing(x):
+    pass
+
+# add a threshold slider
+th1 = 'threshold1'
+cv2.createTrackbar(th1, canny, 0, 255, nothing)
+th2 = 'threshold2'
+cv2.createTrackbar(th2, canny, 0, 255, nothing)
 
 while True:
-	img = cv2.imread('index.png',0)	#save the image in a variable
-	edges = cv2.Canny(img,100,200)	#apply the canny edge filter
+	s = cv2.getTrackbarPos(th1, canny)
+	t = cv2.getTrackbarPos(th2, canny)
 
-	cv2.imshow("source", img)	#show the original image
+    # get the current position of the tracker
+
+	img = cv2.imread('index.png',0)	#save the image in a variable
+	edges = cv2.Canny(img,s,t)	#apply the canny edge filter
+
+	cv2.imshow(canny, edges)	#show the canny filtered image
+
 	k = cv2.waitKey(33)	#delay to handle key presses
 	if k == 27:	#if the escape key is pressed, break the loop
 		break
 	elif k == 32:	#else if space key is pressed
-		cv2.imshow("cannyEdge", edges)	#show the canny filter
-		#todo - check how canny edge filter actually works
-		#apply those filters manually to show the working
+		cv2.imshow("Original Image", img)	#show the original image
